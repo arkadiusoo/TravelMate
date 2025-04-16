ac62820243f039ba8a242599091c40b9256cb539
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-04-16 23:06:21 +0200

    some comments

--- TravelMate/src/main/java/pl/sumatywny/travelmate/budget/repository/ExpenseRepository.java
+++ TravelMate/src/main/java/pl/sumatywny/travelmate/budget/repository/ExpenseRepository.java
@@ -8,4 +8,17 @@ import java.util.UUID;
 
 public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
     List<Expense> findAllByTripId(UUID tripId);
+
+    // already implemented due to JpaReposiotry:
+    //save(S entity)
+    //saveAll(Iterable<S>)
+    //findById(ID id)
+    //existsById(ID id)
+    //findAll()
+    //findAllById(Iterable<ID>)
+    //count()
+    //deleteById(ID id)
+    //delete(Expense entity)
+    //deleteAll()
+    //deleteAll(Iterable<Expense>)
 }
